c3eb2d3be51af26bd84a9210605adcf4d4cf5279
szjaniak <szjaniak1@gmail.com>
2023-10-26 15:44:21 +0200

    parser seems to be done

--- Lista1/mst.py
+++ Lista1/mst.py
@@ -3,7 +3,9 @@ import parser as par
 import sys
 
 def main() -> int:
-	par.parse(str(sys.argv[1]))
+	result = par.parse(str(sys.argv[1]))
+
+	print(result)
 
 if __name__ == '__main__':
     main()
--- Lista1/parser.py
+++ Lista1/parser.py
@@ -1,6 +1,11 @@
 def parse(path: str):
-	file = open(str(sys.argv[1]), 'r')
+	file = open(path)
 	dim, dim_num = file.readline().split(':')
-	num = int(dim_num)
+	size = int(dim_num) + 1
 
-	result = 
+	result = [[0] * size] * size
+	for line in file:
+		v1, v2, weight = line.split(' ')
+		result[int(v1)][int(v2)] = int(weight)
+
+	return result
