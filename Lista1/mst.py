e169517ac39612373b0c97288782fe6c98b2d382
szjaniak <szjaniak1@gmail.com>
2023-10-26 17:34:10 +0200

    okay now parser is definitly working good

--- Lista1/mst.py
+++ Lista1/mst.py
@@ -2,10 +2,61 @@ import parser as par
 
 import sys
 
+MAX_WEIGHT = 9999999999
+result = []
+
+def minimum_key(key: [int], mst_set: [bool], size: int):
+	min = MAX_WEIGHT
+	min_index = 0
+	for v in range(0, size):
+		if mst_set[v] == False and key[v] < min:
+			min = key[v]
+			min_index = v;
+
+	return min_index
+
+def MST(parent: [int], graph: [[int]], size: int):
+	v = [[] * size] * size
+	for i in range(1, size):
+		p = [] * size
+		p.append(parent[i])
+		p.append(i)
+		v.append(p)
+		p.clear()
+
+	return v
+
+# def DFS():
+
+def printMST(parent, graph, size):
+    print("Edge \tWeight")
+    for i in range(1, size):
+        print(parent[i], "-", i, "\t", graph[i][parent[i]])
+
+
+def prim_MST(graph: [[int]], size: int):
+	parent = [0] * size
+	key = [MAX_WEIGHT] * size
+	mst_set = [False] * size
+
+	key[0] = 0
+	parent[0] = -1
+
+	for count in range(0, size - 1):
+		u = minimum_key(key, mst_set, size)
+		mst_set[u] = True
+		for v in range(0, size):
+			if graph[u][v] > 0 and mst_set[v] == False and graph[u][v] < key[v]:
+				parent[v] = u
+				key[v] = graph[u][v]
+
+	v = MST(parent, graph, size)
+	return v
+
 def main() -> int:
 	result = par.parse(str(sys.argv[1]))
-
-	print(result)
+	v = prim_MST(result, len(result))
+	# print(v)
 
 if __name__ == '__main__':
     main()
--- Lista1/parser.py
+++ Lista1/parser.py
@@ -2,10 +2,10 @@ def parse(path: str):
 	file = open(path)
 	dim, dim_num = file.readline().split(':')
 	size = int(dim_num) + 1
-
-	result = [[0] * size] * size
+	result = [[0 for i in range(size)] for j in range(size)]
 	for line in file:
 		v1, v2, weight = line.split(' ')
 		result[int(v1)][int(v2)] = int(weight)
+		result[int(v2)][int(v1)] = int(weight)
 
 	return result
