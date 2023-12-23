f988e686f2bb1dc471410dd60004440c661bb98d
szjaniak <szjaniak1@gmail.com>
2023-12-23 22:23:34 +0100

    small refactor

--- Lista4/main.cpp
+++ Lista4/main.cpp
@@ -9,17 +9,25 @@ namespace fs = std::filesystem;
 auto main(int argc, char*argv[]) -> int
 {
 	const std::string data_directory = "data";
+	std::string file_name;
+	std::string file_path;
+	std::vector<std::vector<weight_type>> graph;
+	std::vector<Point> points;
+	size_t points_count;
 
-    for (const auto& entry : fs::directory_iterator(data_directory)) {
-        if (entry.is_regular_file()) {
-            std::string file_name = entry.path().filename().stem().string();
+    for (const auto& entry : fs::directory_iterator(data_directory))
+    {
+        if (entry.is_regular_file())
+        {
+            file_name = entry.path().filename().stem().string();
             std::cout << file_name << std::endl;
 
-            std::string file_path = "./data/" + file_name + ".tsp";
+            file_path = "./data/" + file_name + ".tsp";
+
             auto result_tuple = parse(file_path);
-			std::vector<std::vector<weight_type>> graph = std::get<0>(result_tuple);
-			std::vector<Point> points = std::get<1>(result_tuple);
-			size_t points_count = std::get<2>(result_tuple);
+			graph = std::get<0>(result_tuple);
+			points = std::get<1>(result_tuple);
+			points_count = std::get<2>(result_tuple);
 
 			for (auto p : points)
 			{
